0bb3eed1b4d079ba276cf0a33cb27fb293b8fca4
Atle Frenvik Sveen <atle@frenviksveen.net>
2015-11-08 22:50:54 +0100

    release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='beertools',
-    version='0.0.2',
+    version='0.0.3',
     author='Atle Frenvik Sveen',
     author_email='atle@frenviksveen.net',
     packages=['beertools', 'beertools.tests', 'beertools.util'],
