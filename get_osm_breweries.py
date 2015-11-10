17797123c08652bc805d52e8f81c2d7aa6aae29b
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-11-10 10:08:36 +0100

    improve overpass queries

--- get_osm_breweries.py
+++ get_osm_breweries.py
@@ -4,8 +4,6 @@ import json
 
 import overpass
 
-from beertools import BreweryNameMatcher
-
 
 def read_json(filename):
     with open(filename, 'r') as infile:
@@ -21,7 +19,20 @@ def parse_bbox(bbox):
     return (south, west, north, east)
 
 
-def get_query(tags, south=None, west=None, north=None, east=None):
+def get_list_items(tags, condition):
+    query = []
+    for key, value in tags.iteritems():
+        query.append('node[%s=%s](%s)' % (key, value, condition))
+        query.append('way[%s=%s](%s)' % (key, value, condition))
+        query.append('relation[%s=%s](%s)' % (key, value, condition))
+    return ';'.join(query)
+
+
+def get_query(tags, country=None, south=None, west=None, north=None, east=None):
+
+    if country is not None:
+        list_items = get_list_items(tags, 'area.search')
+        return 'area["ISO3166-1"="%s"]->.search;(%s)' % (country, list_items)
 
     bbox = None
     if south is not None and west is not None and north is not None and east is not None:
@@ -32,16 +43,11 @@ def get_query(tags, south=None, west=None, north=None, east=None):
             north=north
         )
 
-    query = []
-    for key, value in tags.iteritems():
-        line = 'node[%s=%s]' % (key, value)
-        if bbox:
-            line += '(%s)' % bbox
-        query.append(line)
-    return '(%s)' % ';'.join(query)
+    list_items = get_list_items(tags, bbox)
+    return '(%s)' % (list_items)
 
 
-def parse_brewery(feature):
+def parse_brewery(feature, country):
     old_properties = feature.get('properties', {})
     properties = {
         'id': feature.get('id', None),
@@ -51,14 +57,14 @@ def parse_brewery(feature):
         'city': old_properties.get('addr:city', None),
         'postcode': old_properties.get('addr:postcode', None),
         'street': old_properties.get('addr:street', None),
+        'country': country,
     }
 
     feature['properties'] = properties
     return feature
 
 
-def get_osm_breweries(bbox):
-    south, west, north, east = parse_bbox(bbox)
+def get_osm_breweries(bbox=None, country=None):
 
     api = overpass.API(timeout=600)
 
@@ -68,10 +74,17 @@ def get_osm_breweries(bbox):
         'microbrewery': 'yes',
     }
 
-    q = get_query(tags, south, west, north, east)
+    if bbox is not None:
+        south, west, north, east = parse_bbox(bbox)
+        q = get_query(tags, south=south, west=west, north=north, east=east)
+    elif country is not None:
+        q = get_query(tags, country=country)
+
+    if q is None:
+        return
 
     response = api.Get(q, asGeoJSON=True)
-    features = [parse_brewery(feature) for feature in response['features']]
+    features = [parse_brewery(feature, country) for feature in response['features']]
     return {
         'type': 'featurecollection',
         'features': features
@@ -80,15 +93,10 @@ def get_osm_breweries(bbox):
 
 if __name__ == '__main__':
     # bbox = '-12.612305,55.028022,54.140625,71.413177'
-    bbox = '10.202522,63.360289,10.724030,63.491452'
-    breweries = get_osm_breweries(bbox)
-    rb_breweries = read_json('data/rb_breweries.json')
+    # bbox = '10.202522,63.360289,10.724030,63.491452'
+    # breweries = get_osm_breweries(bbox)
 
-    matcher = BreweryNameMatcher(rb_breweries)
-    for brewery in breweries['features']:
-        brewery_name = brewery['properties']['name']
-        match = matcher.match_name(brewery_name)
-        if match is not None:
-            print '%s - %s' % (brewery_name, match['name'])
-        else:
-            print '%s - %s' % (brewery_name, 'NOMATCH')
+    breweries = get_osm_breweries(country='NO')
+
+    with open('data/osm_breweries.geojson', 'w') as outfile:
+        outfile.write(json.dumps(breweries, indent=4))
